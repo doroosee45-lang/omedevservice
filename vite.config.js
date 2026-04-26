af533832a290a4760d0e3b5cbfdd74c8298fe5b3
doroosee45 <doroosee45@gmail.com>
2026-04-26 22:11:20 +0200

    fix: github pages routing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/omedevservice/', // ⚠️ nom exact du repo
   build: {
-    outDir: 'dist'
+    outDir: 'docs'
   }
 })
