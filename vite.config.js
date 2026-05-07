fbacdb1f026efa2d5d3069e2fbb3befd97802d1f
doroosee45 <doroosee45@gmail.com>
2026-05-07 21:41:56 +0200

    fix assets path

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/omedevservice/', // ⚠️ nom du repo GitHub
-  build: {
-    outDir: 'dist'
-  }
+  base: '/',
 })
